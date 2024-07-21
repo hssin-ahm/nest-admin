cd5f128a74534fdf537cef08b90d6791b593be29
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-21 12:50:57 +0100

    add permissions feature

---
+++ src/permission/permission/permission.controller.ts
@@ -0,0 +1,19 @@
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {PermissionService} from "../../permission/permission/permission.service";
+
+@Controller('permissions')
+export class PermissionController {
+
+    constructor(private permissionService: PermissionService) {
+    }
+
+    @Get()
+    async all(){
+        return this.permissionService.all();
+    }
+
+    @Post()
+    async create(@Body('name') name: string){
+        return this.permissionService.create({name})
+    }
+}

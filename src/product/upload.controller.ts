61dd8418827d8bf270e031948bb043bccd487bf5
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-23 12:40:52 +0100

    Upload image

---
+++ src/product/upload.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadFile(@UploadedFile() file){
+        console.log(file)
+    }
+}

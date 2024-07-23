437c97e73f5f12c42d217e57953dd4b69e6b8322
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-23 14:41:06 +0100

    add order feature

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,11 +3,25 @@ import {AbstractService} from "../common/abstract.service";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Order} from "./order.entity";
 import {Repository} from "typeorm";
+import {PaginationResult} from "../common/pagination-result.interface";
 
 @Injectable()
 export class OrderService extends AbstractService{
     constructor(@InjectRepository(Order) private readonly orderRepository: Repository<Order>) {
         super(orderRepository);
+    }
+
+
+    async paginate(page = 1): Promise<PaginationResult>{
+        const {data, meta} = await super.paginate(page);
+        return {
+            data: data.map((order :Order) =>({
+                id: order.id,
+                name: order.name,
+                total:order.total,
+                created_at: order.created_at,
+            })), meta
+        }
     }
 
 }

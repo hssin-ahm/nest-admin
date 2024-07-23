0b241977f5bd4ae59b0bfa80476fdf7d9b7d8017
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-23 14:23:37 +0100

    add order feature

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -17,10 +17,10 @@ export class OrderController {
     @Post('orders')
     async create(@Body() order: OrderCreateDto){
         const {orderItems, ...data} = order;
-        return this.orderService.create({
-            data,
+        return {
+            ...data,
             orderItems: orderItems.map(id=> ({id}))
-        });
+        };
     }
 
     @Post('orders/orderItem')

bcea4a961a9f70d525b64ed21de4c780e89c2454
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-23 14:35:47 +0100

    add order feature

--- src/order/order.entity.ts
+++ src/order/order.entity.ts
@@ -24,4 +24,9 @@ export class Order{
     get name(): string{
         return `${this.first_name} ${this.last_name}`;
     }
+
+    @Expose()
+    get total(): number{
+        return this.orderItems.reduce((sum, i)=> sum + i.quantity * i.price,0)
+    }
 }

163b6892773055236c3d6ea18f4a56f006c28c44
“hssinee” <ahmed.hassine@esprit.tn>
2024-07-23 12:58:05 +0100

    Upload image

--- src/product/upload.controller.ts
+++ src/product/upload.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 @Controller('upload')
 export class UploadController {
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('image'))
-    uploadFile(@UploadedFile() file){
-        console.log(file)
+    @Post()
+    @UseInterceptors(FileInterceptor('image', {
+        storage: diskStorage({
+            destination: './uploads',
+            filename: (req, file, callback) => {
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+                const ext = extname(file.originalname);
+                const filename = `${file.fieldname}-${uniqueSuffix}${ext}`;
+                callback(null, filename);
+            },
+        }),
+    }))
+    uploadFile(@UploadedFile() file : Express.Multer.File){
+        return {
+            url: `http://192.168.107.141:3000/api/${file.path}`
+        }
     }
 }
